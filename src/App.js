3cc9e98b37ea7058e790d2e739ba3bfff936b730
Your Name <you@example.com>
2022-04-03 23:32:46 +0300

    routes, active-link and header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from '../src/components/Home/Home';
+import About from '../src/components/About/About';
+import Reviews from '../src/components/Reviews/Reviews';
+import Dashboard from '../src/components/Dashboard/Dashboard';
+import Blogs from '../src/components/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="about" element={<About></About>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/reviews" element={<Reviews></Reviews>} />
+        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+      </Routes>
       
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assest/logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h3>Here is header</h3>
+        <div className='d-flex align-items-center justify-content-around '>
+            <div className="logo-container d-flex justify-content-center align-items-center">
+                <div className="logo">
+                    <img src={logo} alt="" srcset="" />
+                </div>
+                <div className="name ms-2">
+                    <h4>Campus</h4>
+                </div>
+            </div>
+            <div className="nav-link">
+                <NavLink
+                    className={({ isActive }) => (isActive ? "active-link" : "link")}
+                    to='/'
+                >
+                    HOME
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "active-link" : "link")}
+                    to='/reviews'
+                >
+                    REVIEWS
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "active-link" : "link")}
+                    to='/dashboard'
+                >
+                    DASHBOARD
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "active-link" : "link")}
+                    to='/blogs'
+                >
+                    BLOGS
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "active-link" : "link")}
+                    to='/about'
+                >
+                    ABOUT
+                </NavLink>
+
+            </div>
         </div>
     );
 };
